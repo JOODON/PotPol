ec732ba1265f90cb029eedf5c69b7b57989c47ef
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-25 11:27:06 +0900

    ㅇㅁㅇㅁ

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 $(function (){
     $(`.Contact`).click(function (){
         $(`.Board`).css({display:`block`}),
-        $(`.h2`).css({display:`none`});
+        $(`.main-page`).css({display:`none`});
 
-        $(`.Search`).click(function (){
+        $(`.main`).click(function (){
             $(`.Board`).css({display:`none`},
-                $(`.h2`).css({display:`block`}));
+                $(`.main-page`).css({display:`block`}));
         })
     })
 })
